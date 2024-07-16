6095512b99e4fb339369c83d1b0d736183588a3d
marianalema <62489409+marianalema@users.noreply.github.com>
2024-07-16 14:13:26 -0300

    Create render_to_file.py

---
+++ render_to_file.py
@@ -0,0 +1,14 @@
+
+from app import app, generate_captcha
+
+@app.route('/render_login')
+def render_login():
+    captcha_question, captcha_answer = generate_captcha()
+    rendered_html = render_template('login.html', captcha_question=captcha_question)
+    with open('docs/login.html', 'w') as file:
+        file.write(rendered_html)
+    return 'Login page rendered to docs/login.html'
+
+if __name__ == '__main__':
+    with app.app_context():
+        app.run(debug=True)
