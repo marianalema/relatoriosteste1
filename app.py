81fea16612036a65229d4de0f69aa95b21161d0b
marianalema <62489409+marianalema@users.noreply.github.com>
2024-07-23 10:06:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,28 +2,53 @@ from flask import Flask, render_template, redirect, url_for, flash, request, ses
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 import os
+import csv
 import random
+from models import db, User
+import config
+import mysql.connector
+import logging
 
 app = Flask(__name__)
-app.config.from_object('config.Config')
+app.config.from_object(config.Config)
 
-db = SQLAlchemy(app)
+db.init_app(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), nullable=False, unique=True)
-    password = db.Column(db.String(150), nullable=False)
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-@app.route('/')
 @app.route('/index')
+@app.route('/')
 def index():
     return redirect(url_for('login'))
+
+def reset_database():
+    db.drop_all()
+    db.create_all()
+
+def initialize_users():
+    if not os.path.exists('usuarios.csv'):
+        print("O arquivo usuarios.csv não foi encontrado.")
+        return
+
+    try:
+        with open('usuarios.csv', newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            next(reader)  # Skip the header row
+            for row in reader:
+                if len(row) != 2:
+                    print(f"Formato inválido na linha: {row}")
+                    continue
+                username, password = row
+                if not User.query.filter_by(username=username).first():
+                    new_user = User(username=username, password=password)
+                    db.session.add(new_user)
+            db.session.commit()
+    except Exception as e:
+        print(f"Erro ao ler o arquivo CSV: {e}")
 
 def generate_captcha():
     num1 = random.randint(1, 9)
@@ -39,52 +64,110 @@ def login():
         password = request.form.get('password')
         captcha_response = request.form.get('captcha')
 
+        # Verifica o captcha
         if 'captcha_answer' not in session or int(captcha_response) != session['captcha_answer']:
-            flash('Invalid captcha. Please try again.')
+            flash('Captcha inválido. Tente novamente.')
             return redirect(url_for('login'))
 
-        # Debug print statements
-        print(f"Attempting login with username: {username}")
+        user = User.query.filter_by(username=username).first()
 
-        try:
-            user = User.query.filter_by(username=username).first()
-        except Exception as e:
-            app.logger.error(f"Error querying the database: {e}")
-            flash('An error occurred while processing your request.')
-            return redirect(url_for('login'))
-
-        if user:
-            print(f"User found: {user.username}")
-            if user.password == password:
-                login_user(user)
-                return redirect(url_for('dashboard_view'))
-            else:
-                flash('Incorrect password.')
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('dashboard_view'))
         else:
-            flash('Username not found.')
+            flash('Esqueceu o login? ')
 
+    # Gera uma nova conta para o captcha
     captcha_question, captcha_answer = generate_captcha()
     session['captcha_answer'] = captcha_answer
 
     return render_template('login.html', captcha_question=captcha_question)
 
+def get_faturamento_publicidade(email):
+    try:
+        connection = mysql.connector.connect(
+            host="database-1.cedwytewklqg.us-east-1.rds.amazonaws.com",
+            user="admin",
+            password="Curta123",
+            database="curta_analytics"
+        )
+        cursor = connection.cursor()
+        query = """
+        SELECT
+            reports.influenciador,
+            reports.data,
+            reports.marca,
+            reports.campanha,
+            reports.valor_influenciador,
+            reports.status_pgt_,
+            pic.picture
+        FROM
+            curta_analytics.reports_influ_beta2 AS reports
+        LEFT JOIN
+            curta_analytics.influenciadores_mkdigital_2024 AS pic
+        ON
+            reports.influenciador = pic.name
+        WHERE
+            reports.e_mail_influ = %s
+        LIMIT 100;
+        """
+        cursor.execute(query, (email,))
+        results = cursor.fetchall()
+        logging.debug(f"Number of records fetched: {len(results)}")
+        logging.debug(f"Data retrieved: {results}")
+        cursor.close()
+        connection.close()
+
+        if results:
+            influenciador = results[0][0]
+            picture = results[0][6]
+            data = [row[1:6] for row in results]
+            
+            total_a_receber = 0
+            total_recebido = 0
+            
+            for row in results:
+                valor = row[4].replace('R$', '').replace(',', '').strip()
+                valor_float = float(valor) if valor else 0.0
+                
+                if row[5] == "Aguardando":
+                    total_a_receber += valor_float
+                elif row[5] == "OK":
+                    total_recebido += valor_float
+
+            return influenciador, data, total_a_receber, total_recebido, picture
+        else:
+            return None, [], 0.0, 0.0, None
+
+    except mysql.connector.Error as err:
+        logging.error(f"Error: {err}")
+        return None, [], 0.0, 0.0, None
+
+
+
+@app.template_filter('currency_format')
+def currency_format(value):
+    if isinstance(value, str):
+        try:
+            value = float(value.replace('R$', '').replace(',', '').strip())
+        except ValueError:
+            return value  # In case the value is not a number, return it as is
+    return f"R$ {value:,.2f}"
+
+
 @app.route('/dashboard')
 @login_required
 def dashboard_view():
-    email = current_user.username
-    # Assuming get_faturamento_publicidade is implemented elsewhere in your code
+    email = current_user.username  # Assuming the username is the email
+    logging.debug(f"Email do usuário logado: {email}")
     influenciador, data, total_a_receber, total_recebido, picture = get_faturamento_publicidade(email)
-
-    power_bi_embed_url = "https://app.powerbi.com/reportEmbed?reportId=3b05e3a8-e10b-4f63-8328-1241ef31e17e&autoAuth=true&ctid=3efff910-7dd6-45b1-ab3c-6f7351e8346a"
-
-    return render_template('dashboard.html',
-                           user=current_user,
-                           influenciador=influenciador,
-                           data=data,
-                           total_a_receber=total_a_receber,
-                           total_recebido=total_recebido,
-                           picture=picture,
-                           power_bi_embed_url=power_bi_embed_url)
+    logging.debug(f"Influenciador: {influenciador}, Data passed to template: {data}, Total a Receber: {total_a_receber}, Total Recebido: {total_recebido}, Picture: {picture}")
+    
+    # Prepare data for the chart
+    dates = [row[0] for row in data]
+    values = [float(row[3].replace('R$', '').replace(',', '').strip()) for row in data]
+    
+    return render_template('dashboard.html', user=current_user, influenciador=influenciador, data=data, total_a_receber=total_a_receber, total_recebido=total_recebido, picture=picture, dates=dates, values=values)
 
 @app.route('/logout')
 @login_required
@@ -93,6 +176,8 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     with app.app_context():
-        db.create_all()  # Ensure the database tables are created
+        reset_database()
+        initialize_users()
     app.run(debug=True)
